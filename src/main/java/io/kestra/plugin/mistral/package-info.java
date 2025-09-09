04732edc0e85b82b09fa801089280ca2d47c1a0a
François Delbrayelle <fdelbrayelle@gmail.com>
2025-09-09 11:13:18 +0200

    fix(): add missing category to AI

---
+++ src/main/java/io/kestra/plugin/mistral/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Mistral plugin",
+    description = "A plugin to use Mistral.",
+    categories = PluginSubGroup.PluginCategory.AI
+)
+package io.kestra.plugin.mistral;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
